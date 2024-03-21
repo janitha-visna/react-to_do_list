291f088b24bcb2b4e4e7bb3775660efdad206144
janitha-visna <visnajanitha@gmail.com>
2024-03-21 16:32:22 +0530

    saving tasks to local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-
-import { useState } from 'react';
-import './App.css';
-import Task from './Task';
-import TaskForm from './TaskForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+  }, []);
+
   function addTask(name) {
-    setTasks( prev => {
-      return [...prev,{name:name,done:false}];
-    })
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
   }
+
   return (
     <main>
       <TaskForm onAdd={addTask} />
-      {tasks.map(task => (
-        <Task {...task} />
+      {tasks.map((task, index) => (
+        <Task key={index} {...task} />
       ))}
-
     </main>
   );
 }
